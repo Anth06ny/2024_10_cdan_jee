0df3564cbc1208eac99402aabcde35af736f035a
AnthonyPCPortable <anth06ny@gmail.com>
2025-04-10 12:30:54 +0200

    Fichier manquant

---
+++ ProgReactive/src/main/kotlin/org/example/progreactive/ProgReactiveApplication.kt
@@ -0,0 +1,11 @@
+package org.example.progreactive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProgReactiveApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProgReactiveApplication>(*args)
+}
